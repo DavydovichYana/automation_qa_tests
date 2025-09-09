c1329841e4aec49d9de8405245da03c3b15a7c98
Yana Davydovich <56787449+DavydovichYana@users.noreply.github.com>
2025-09-09 16:04:32 +0300

    update elements_test added tests: -test_web_table_add_person -test_web_table_search_person

--- generator/generator.py
+++ generator/generator.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from data.data import Person
 from faker import Faker
 
@@ -7,6 +9,11 @@ Faker.seed()
 def generated_person():
     yield Person(
         full_name = faker_ru.first_name() + " " + faker_ru.last_name() + " " + faker_ru.middle_name(),
+        firstname=faker_ru.first_name(),
+        lastname=faker_ru.last_name(),
+        age=randint(10, 80),
+        department=faker_ru.job(),
+        salary=randint(10000, 800000),
         email = faker_ru.email(),
         current_address = faker_ru.address(),
         permanent_address = faker_ru.address(),
