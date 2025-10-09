aaf13a7735eb4350deced924ef418c7cba216db2
Yana Davydovich <56787449+DavydovichYana@users.noreply.github.com>
2025-10-09 11:37:55 +0300

    added test_sortable

---
+++ locators/interactions_page_locators.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+
+class SortablePageLocators:
+    TAB_LIST = (By.ID, 'demo-tab-list')
+    LIST_ITEM = (By.CSS_SELECTOR, 'div[id="demo-tabpane-list"] div[class="list-group-item list-group-item-action"]')
+
+    TAB_GRID = (By.ID, 'demo-tab-grid')
+    GRID_ITEM = (By.CSS_SELECTOR, 'div[id="demo-tabpane-grid"] div[class="list-group-item list-group-item-action"]')
