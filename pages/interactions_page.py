10c1598129b886f30d2c1e7ef4a3a85706c98b0d
Yana Davydovich <56787449+DavydovichYana@users.noreply.github.com>
2025-10-09 12:07:30 +0300

    added item_grid

--- pages/interactions_page.py
+++ pages/interactions_page.py
@@ -1,6 +1,6 @@
 import random
 
-from locators.interactions_page_locators import SortablePageLocators
+from locators.interactions_page_locators import SortablePageLocators, SelectablePageLocators
 from pages.base_page import BasePage
 
 
@@ -11,24 +11,52 @@ class SortablePage(BasePage):
         item_list = self.elements_are_visible(elements)
         return [item.text for item in item_list]
 
-    def change_list_order(self):
-        self.element_is_visible(self.locators.TAB_LIST).click()
-        order_before = self.get_sortable_items(self.locators.LIST_ITEM)
-        item_list = random.sample(self.elements_are_visible(self.locators.LIST_ITEM), k=2)
+    def change_order(self, tab):
+        tabs = {
+            'list': {
+                'tab': self.locators.TAB_LIST,
+                'item': self.locators.LIST_ITEM,
+            },
+            'grid': {
+                'tab': self.locators.TAB_GRID,
+                'item': self.locators.GRID_ITEM,
+            }
+        }
+        self.element_is_visible(tabs[tab]['tab']).click()
+        order_before = self.get_sortable_items(tabs[tab]['item'])
+        item_list = random.sample(self.elements_are_visible(tabs[tab]['item']), k=2)
         item_what = item_list[0]
         item_where = item_list[1]
         self.action_drag_and_drop_to_element(item_what, item_where)
-        order_after = self.get_sortable_items(self.locators.LIST_ITEM)
+        order_after = self.get_sortable_items(tabs[tab]['item'])
         print(order_before, order_after)
         return order_before, order_after
 
-    def change_grid_order(self):
-        self.element_is_visible(self.locators.TAB_GRID).click()
-        order_before = self.get_sortable_items(self.locators.GRID_ITEM)
-        item_list = random.sample(self.elements_are_visible(self.locators.GRID_ITEM), k=2)
-        item_what = item_list[0]
-        item_where = item_list[1]
-        self.action_drag_and_drop_to_element(item_what, item_where)
-        order_after = self.get_sortable_items(self.locators.GRID_ITEM)
-        print(order_before, order_after)
-        return order_before, order_after
+
+class SelectablePage(BasePage):
+    locators = SelectablePageLocators()
+
+    def click_selectable_items(self, elements):
+        item_list = self.elements_are_visible(elements)
+        random.sample(item_list, k=1)[0].click()
+
+    def select_list_item(self, tab):
+        tabs = {
+            'list': {
+                'tab': self.locators.TAB_LIST,
+                'item': self.locators.LIST_ITEM,
+                'item_active': self.locators.LIST_ITEM_ACTIVE,
+            },
+            'grid': {
+                'tab': self.locators.TAB_GRID,
+                'item': self.locators.GRID_ITEM,
+                'item_active': self.locators.GRID_ITEM_ACTIVE,
+            }
+        }
+        self.element_is_visible(tabs[tab]['tab']).click()
+        self.click_selectable_items(tabs[tab]['item'])
+        active_element = self.element_is_visible(tabs[tab]['item_active']).text
+        return active_element
+
+
+
--- tests/interactions_test.py
+++ tests/interactions_test.py
@@ -1,4 +1,4 @@
-from pages.interactions_page import SortablePage
+from pages.interactions_page import SortablePage, SelectablePage
 
 
 class TestInteractions:
@@ -6,7 +6,15 @@ class TestInteractions:
     def test_sortable(self, driver):
         sortable_page = SortablePage(driver, 'https://demoqa.com/sortable')
         sortable_page.open()
-        list_before, list_after = sortable_page.change_list_order()
-        grid_before, grid_after = sortable_page.change_grid_order()
+        list_before, list_after = sortable_page.change_order('list')
+        grid_before, grid_after = sortable_page.change_order('grid')
         assert list_before != list_after,'Порядок элементов списка не изменился'
         assert grid_before != grid_after, 'Порядок элементов таблицы не изменился'
+
+    def test_selectable(self, driver):
+        selectable_page = SelectablePage(driver, 'https://demoqa.com/selectable')
+        selectable_page.open()
+        item_list = selectable_page.select_list_item('list')
+        item_grid = selectable_page.select_list_item('grid')
+        assert len(item_list) > 0, 'Элемент в списке не выбран'
+        assert len(item_grid) > 0, 'Элемент в таблице не выбран'
